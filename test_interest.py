329ffd15d99edf34be5e86ef7b217b07b94bed72
briN <webryan2k@gmail.com>
2017-04-03 23:57:50 +0300

    fourth commit

--- test_interest.py
+++ test_interest.py
@@ -4,7 +4,7 @@ from interest import loan_calculator
 class loan_calculator(unittest.TestCase):
 	def testloan_calculator(self):
 		self.assertEquals(loan_calculator(100000, 12, 12), 112000)
-	# def test_more_than_year(self):
+	def test_more_than_year(self):
 		#self.assertEqual(loan_calculator(13),'Return invalid time')
 
 
