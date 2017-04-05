816af6dccde2d1b8d1b0841d46c1d6e0548d677e
briN <webryan2k@gmail.com>
2017-04-05 19:21:00 +0300

    Command line application

--- api.py
+++ api.py
@@ -1,16 +1,16 @@
 import urllib.request
 import json
 
+ 
 zomato_api = 'c7d979db7c4e418be68fd3b9a7d90115'
-
-
-def zomato_search(query):
-	api_key = zomato_api
-	url = 'https://developers.zomato.com/api/v2.1/search?api_key' + api_key
-	location = query
-	final_url = url + "&location=" + location + "&entity_type=city=" + "&collection_id=1"
-    json_obj = urllib.request.urlopen(final_url)
-	data = json.load(json_obj)
-
-	for item in data["restaurants"]:
-		print(" This weeks trending restaurants in New York:",["name"],["address"],["cuisines"])
+ 
+def locu_search(query):
+    api_key = zomato_api
+    url = 'https://developers.zomato.com/api/v2.1/search?api_key' + api_key
+    locality = query.replace(' ', '%20')
+    final_url = url + "&location=" + + location + "&entity_type=city=" + "&collection_id=1"
+    json_obj = urllib2.urlopen(final_url)
+    data = json.load(json_obj)
+   
+    for item in data['restaurants']:
+        print (item['name'], item['address'])
