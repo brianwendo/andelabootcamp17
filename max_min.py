504058f4ba660fd800457ecb812813a75d5463fb
briN <webryan2k@gmail.com>
2017-04-05 13:44:55 +0300

    Andelabs Max_min fucntion

---
+++ max_min.py
@@ -0,0 +1,11 @@
+def max_min():
+	old_list = []
+	for num in old_list: 
+		new_list = []
+		min_value = min(old_list)
+		max_value = max(old_list)
+		new_list = new_list.append(min_value)
+		new_list = new_list.append(max_value)
+		return new_list
+
+
