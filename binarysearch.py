162e4a66ca64bd0d7d3d087c2f34b83315f2cbb7
briN <webryan2k@gmail.com>
2017-04-06 16:40:43 +0300

    Andelabs Binary Search

---
+++ binarysearch.py
@@ -0,0 +1,31 @@
+
+
+class BinarySearch():
+	"""Binary Search to traverse an ordered list, effectively
+       Populate the arrays with valid content
+    """
+sample_list = []
+
+def __init__(self,a,b):
+	length = len(sample_list)
+	a = length
+	self.a = a
+	self.b = b
+	return a
+	"""a is the length of the list"""
+	"""b is the step value between consecutive values"""
+
+def search(self,a):
+
+	count = {}
+	for element in sample_list:
+		if element in sample_list:
+			count[element] += 1
+		else:
+			count[element] = 1
+			return count
+
+def __str__(self):
+    return 'a: {0}, count: {1}'.format(self.a, self.count)
+
+
